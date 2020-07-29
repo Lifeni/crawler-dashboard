582dd9bf72d212877553dfe4b6cee24d0376c2e1
Lifeni <liangfengning@foxmail.com>
2020-07-29 10:30:15 +0800

    完成基本 API 的编码

--- backend/server/models.py
+++ backend/server/models.py
@@ -1,3 +1,53 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+
+class Pictures(models.Model):
+    picture_url = models.CharField(max_length=255)
+    picture_date = models.CharField(max_length=255)
+    picture_description = models.CharField(max_length=255)
+    picture_like = models.IntegerField()
+    picture_download = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'pictures'
+
+
+class Poems(models.Model):
+    poem_author = models.ForeignKey('PoemsAuthors', models.DO_NOTHING, db_column='poem_author')
+    poem_title = models.CharField(max_length=255)
+    poem_title_id = models.CharField(max_length=255)
+    poem_content = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'poems'
+
+
+class PoemsAuthors(models.Model):
+    author_name = models.CharField(unique=True, max_length=255)
+    author_description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'poems_authors'
+
+
+class Sentences(models.Model):
+    sentence_id = models.PositiveIntegerField(primary_key=True)
+    sentence_category = models.CharField(max_length=255)
+    sentence_category_description = models.CharField(max_length=255)
+    sentence_content = models.CharField(max_length=255)
+    sentence_from = models.CharField(max_length=255, blank=True, null=True)
+    sentence_from_who = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'sentences'
--- backend/server/views.py
+++ backend/server/views.py
@@ -1,3 +1,77 @@
+import json
+
+from django.db.models import Count, Sum, Avg, Max, Min
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
+from .models import Pictures
+from .models import Poems
+from .models import PoemsAuthors
+from .models import Sentences
+
+
 # Create your views here.
+
+
+def index(request):
+    pictures_json = {
+        'data_count': Pictures.objects.count(),
+        'total_like_count': Pictures.objects.aggregate(Sum('picture_like'))['picture_like__sum'],
+        'avg_like_count': Pictures.objects.aggregate(Avg('picture_like'))['picture_like__avg'],
+        'max_like_count': Pictures.objects.aggregate(Max('picture_like'))['picture_like__max'],
+        'max_like_count_image_url': Pictures.objects.order_by('-picture_like')[0].picture_url,
+        'max_like_count_image_text': Pictures.objects.order_by('-picture_like')[0].picture_description,
+        'min_like_count': Pictures.objects.aggregate(Min('picture_like'))['picture_like__min'],
+        'total_download_count': Pictures.objects.aggregate(Sum('picture_download'))['picture_download__sum'],
+        'avg_download_count': Pictures.objects.aggregate(Avg('picture_download'))['picture_download__avg'],
+        'max_download_count': Pictures.objects.aggregate(Max('picture_download'))['picture_download__max'],
+        'max_download_count_image_url': Pictures.objects.order_by('-picture_download')[0].picture_url,
+        'max_download_count_image_text': Pictures.objects.order_by('-picture_download')[0].picture_description,
+        'min_download_count': Pictures.objects.aggregate(Min('picture_download'))['picture_download__min'],
+        'num_data': list(Pictures.objects.all().values('picture_date', 'picture_like', 'picture_download'))
+    }
+
+    author_poem_list = Poems.objects.values('poem_author').annotate(Count('poem_author')).all().order_by('-poem_author__count')
+    poem_title_list = Poems.objects.values('poem_title').annotate(Count('poem_title')).all().order_by('-poem_title__count')
+
+    poems_json = {
+        'data_count': Poems.objects.count(),
+        'author_data_count': PoemsAuthors.objects.count(),
+        'author_count': Poems.objects.values('poem_author').distinct().count(),
+        'title_count': Poems.objects.values('poem_title').distinct().count(),
+        'author_poem_data': list(author_poem_list),
+        'author_avg_poem_count': Poems.objects.count() / PoemsAuthors.objects.count(),
+        'poem_title_data': list(poem_title_list),
+        'poem_avg_title_count': Poems.objects.count() / Poems.objects.values('poem_title').distinct().count(),
+        'poem_count_1': author_poem_list.filter(poem_author__count=1).count(),
+    }
+
+    sentences_json = {
+        'data_count': Sentences.objects.count(),
+        'sentence_data': list(Sentences.objects.values('sentence_category').annotate(Count('sentence_category')).all().order_by('-sentence_category__count'))
+    }
+
+    overview_json = {
+        'total_data_count': pictures_json['data_count'] + poems_json['data_count'] + poems_json['author_data_count'] + sentences_json['data_count'],
+        'category_count': 3,
+        'database_table_count': 4,
+        'pictures_data_count': pictures_json['data_count'],
+        'pictures_image_count': pictures_json['data_count'],
+        'pictures_total_like_count': pictures_json['total_like_count'],
+        'pictures_total_download_count': pictures_json['total_download_count'],
+        'poems_data_count': poems_json['data_count'] + poems_json['author_data_count'],
+        'poems_poem_count': poems_json['data_count'],
+        'poems_author_count': poems_json['author_data_count'],
+        'sentences_data_count': sentences_json['data_count'],
+        'sentences_try_count': 6500,
+        'sentences_category_count': 8,
+    }
+
+    return JsonResponse({
+        'code': '0',
+        'message': 'ok',
+        'overview': overview_json,
+        'pictures': pictures_json,
+        'poems': poems_json,
+        'sentences': sentences_json,
+    })
